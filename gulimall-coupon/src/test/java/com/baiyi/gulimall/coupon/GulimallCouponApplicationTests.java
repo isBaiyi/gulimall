ecb17907f02186f99888633780cc82a9a35eaec4
liaozicai <1101293873@qq.com>
2023-11-09 16:33:02 +0800

    feat:初始化项目

---
+++ gulimall-coupon/src/test/java/com/baiyi/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baiyi.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
